3d0a12d5c824bc12deed79b451153c0d2e71c849
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-03-25 15:44:04 -0300

    Criação da classe delegate bank para que ela trabalhe com as operações da camada de serviço

---
+++ Bank/Service/BankDelegate.cs
@@ -0,0 +1,45 @@
+﻿using Bank.Controllers;
+using Bank.Data;
+using Bank.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Service
+{
+    internal class BankDelegate
+    {
+        public delegate void BusinessOperation(Dictionary<int, Account> accounts, BusinessAccount businessAccount);
+        public delegate void SavingsOperation(Dictionary<int, Account> accounts, SavingAccount savingsAccount);
+        public delegate void MenuOption(Dictionary<int, Account> accounts);
+
+        public static Dictionary<int, BusinessOperation> BusinessOperations = new Dictionary<int, BusinessOperation>
+        {
+            {1, (account, acc) => Deposit.PerformDeposit(acc, new ConsoleMessageService())},
+            {2, (account, acc) => Withdraw.PerformWithdraw(acc, new ConsoleMessageService())},
+            {3, (account, acc) => Loan.PerformLoan(acc, new ConsoleMessageService())},
+            {4, (account, acc) => UpdateLoanLimit.PerformUpdateLoanLimit(acc)},
+            {5, (account, acc) => Transfer.TransferAccount(account, acc, new ConsoleMessageService())}
+        };
+
+        public static Dictionary<int, SavingsOperation> SavingsOperations = new Dictionary<int, SavingsOperation>
+        {
+            {1, (account, acc) => Deposit.PerformDeposit(acc, new ConsoleMessageService())},
+            {2, (account, acc) => Withdraw.PerformWithdraw(acc, new ConsoleMessageService())},
+            {3, (account, acc) => acc.UpdateBalance()},
+            {4, (account, acc) => Transfer.TransferAccount(account, acc, new ConsoleMessageService())}
+        };
+
+        public static Dictionary<int, MenuOption> MenuOperations = new Dictionary<int, MenuOption>
+        {
+            {1, (account) => Create.CreateAccount(account)},
+            {2, (account) => PerformAccount.PerformAccountOperation(account)},
+            {3, (account) => Display.DisplayAccount(account)},
+            {4, (account) => Delete.DeleteAccount(account)}
+        };
+
+
+    }
+}
